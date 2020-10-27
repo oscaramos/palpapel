a215cc586c31d613983340f83549ccd8bbe04f62
Oscar <oscar.ramos@ucsp.edu.pe>
2020-10-27 00:18:07 -0500

    ♻ Rename orderControl/ page to order/

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { GlobalStyles } from './globalStyles'
 import { useDarkMode } from './hooks/useDarkMode'
 import { Route, Switch } from 'wouter'
 import Home from './pages/Home'
-import OrderControl from './pages/OrderControl'
+import Order from './pages/Order'
 
 function App() {
   const [theme] = useDarkMode()
@@ -18,7 +18,7 @@ function App() {
       <GlobalStyles />
       <Switch>
         <Route path='/' component={Home} />
-        <Route path='/orderControl' component={OrderControl} />
+        <Route path='/order' component={Order} />
       </Switch>
     </ThemeProvider>
   );
