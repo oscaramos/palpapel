4bea5fe5a49233412932a2aa0cdb5d69e870c0ed
Oscar <oscar.ramos@ucsp.edu.pe>
2020-11-23 02:04:39 -0500

    ♻ Remove global variable with dependency injection

--- src/utils/user.firebase.js
+++ src/utils/user.firebase.js
@@ -1,10 +1,6 @@
-import { auth, firestore } from "../firebase.utils"
+import { firestore } from "./firebase.utils"
 
-export async function updateUser(data) {
-  const userRef = firestore.collection("users").doc(auth.currentUser.uid)
-  await userRef.update(data)
-}
-
-export async function editUserDataFilters(filters) {
-  await updateUser({ filters })
+export async function editUserDataFilters(uid, filters) {
+  const userRef = firestore.collection("users").doc(uid)
+  await userRef.update({ filters })
 }
